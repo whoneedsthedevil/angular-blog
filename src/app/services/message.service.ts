f6bba50e3e86c5403ea1e8d4b5963720fd1c9423
Dear Friend <pavel.abramov.spb@gmail.com>
2017-12-21 06:33:21 +1100

    add api-alerts UI

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -3,9 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MessageService {
   messages: string[] = [];
+  currentMessage: string;
+  isActive: boolean = false;
  
   add(message: string) {
+  	this.isActive = true;
     this.messages.push(message);
+    this.currentMessage = message;
+    setTimeout(() => (this.isActive = false), 3000)
   }
  
   clear() {
